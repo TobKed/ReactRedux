6f5a73875a7fd917747963c453ae3bfb70ef542b
TobKed <tobiaszkedzierski@gmail.com>
2019-02-20 20:52:35 +0100

    map over list to get sequence of components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react';
 import Ninjas from './Ninjas';
 
 class App extends Component {
+  state = {
+    ninjas: [
+      {name: "Justin", age:16, belt: "pink", id: 1},
+      {name: "Chuck", age:99, belt: "Black", id: 2},
+      {name: "Borixon", age:45, belt: "silver", id: 3}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>My first React App!</h1>
         <p>Welcome my friend :)</p>
-        <Ninjas name="Justin" age="16" belt="pink"/>
-        <Ninjas name="Chuck" age="99" belt="black"/>
+        <Ninjas ninjas={ this.state.ninjas }/>
       </div>
     );
   }
