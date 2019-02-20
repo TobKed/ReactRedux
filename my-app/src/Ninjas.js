e5682901f8bc5fc34dfe765e87653622de8e8529
TobKed <tobiaszkedzierski@gmail.com>
2019-02-20 20:18:16 +0100

    start react-app

---
+++ my-app/src/Ninjas.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Ninjas extends Component {
+    render() {
+        return (
+            <div className="ninja">
+                <div>Name: Justin</div>
+                <div>Age: 16</div>
+                <div>Belt: Pink</div>
+            </div>
+        )
+    }
+}
+
+export default Ninjas
