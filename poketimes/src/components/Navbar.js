4145578235585d874646b587c7afae20029ffdc9
TobKed <tobiaszkedzierski@gmail.com>
2019-02-22 17:56:19 +0100

    add programmatic redirect, higher order elements

--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React from 'react'
+import { Link, NavLink, withRouter } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = (props) => {
+  setTimeout(() => {
+        props.history.push('/about')
+  }, 2000)
   return (
     <nav className="nav-wrapper red darken-3">
       <div className="container">
-        <a className="brand-logo">Poke' Times</a>
+        <a className="brand-logo">Poke'Times</a>
         <ul className="right">
-          <li><a href="/">Home</a></li>
-          <li><a href='/about'>About</a></li>
-          <li><a href='/contact'>Contact</a></li>
+          <li><Link to="/">Home</Link></li>
+          <li><NavLink to='/about'>About</NavLink></li> {/* add class "active" */}
+          <li><NavLink to='/contact'>Contact</NavLink></li> {/*  add class "active" */}
         </ul>
       </div>
     </nav> 
   )
 }
 
-export default Navbar
+export default withRouter(Navbar)  // supercharging component
