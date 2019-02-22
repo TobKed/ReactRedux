ccd26024e213057441030740e68f99340594d196
TobKed <tobiaszkedzierski@gmail.com>
2019-02-22 20:09:11 +0100

    add route for single post

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 class Home extends Component {
     state = {
@@ -8,7 +9,6 @@ class Home extends Component {
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(res => {
-                console.log(res)
                 this.setState({
                     posts: res.data.slice(0, 10)
                 })
@@ -22,8 +22,10 @@ class Home extends Component {
                 return (
                     <div className="post card" key={post.id}>
                         <div className="card-content">
-                            <span className="card-title">{post.title}</span>
-                            <p>{post.body}</p>
+                            <Link to={'/'+post.id}>
+                                <span className="card-title">{post.title}</span>
+                                <p>{post.body}</p>
+                            </Link>
                         </div>
                     </div>
                 )
