f9c4aba5433c9a864f4c4987553453fe4a0687fa
TobKed <tobiaszkedzierski@gmail.com>
2019-02-22 17:37:58 +0100

    add subpages components

---
+++ poketimes/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, aperiam itaque vel at asperiores suscipit a eum quibusdam repellendus distinctio corporis vitae dignissimos corrupti officia atque assumenda quidem impedit aut.</p>
+        </div>
+    )
+}
+
+export default About
---
+++ poketimes/src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, aperiam itaque vel at asperiores suscipit a eum quibusdam repellendus distinctio corporis vitae dignissimos corrupti officia atque assumenda quidem impedit aut.</p>
+        </div>
+    )
+}
+
+export default Contact
